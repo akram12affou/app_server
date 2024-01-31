f37508cc464ff2c6b9f950249d4d84a4a8a52f35
Akram Affou <97305449+akram12affou@users.noreply.github.com>
2024-01-31 06:51:37 +0100

    'change'

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRouter from './routes/userRoutes.js';
 const app = express();
 dotenv.config();
+app.use('/user' , userRouter)
 const PORT = process.env.PORT;
 app.listen(PORT || 3000 , () => {
-    console.log(PORT);
-})
+    connectDB()
+    console.log('running server in port : ' , PORT);
+}) 
+ 
